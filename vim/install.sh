df1945a74ee6ae5b6647095edf92beb0428e7421
Xavier Roumegue <xroumegue@gmail.com>
2017-12-31 00:43:49 +0100

    Add vim
    Signed-off-by: Xavier Roumegue <xroumegue@gmail.com>

---
+++ vim/install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+
+INSTALLDIR="$(dirname "$(readlink -f "$0")")"
+echo "Install dir is $INSTALLDIR"
+
+if [ ! -f ~/.vim ]; then
+    echo "Now, we will create ~/.vim and ~/.vimrc files to configure Vim."
+    ln -sfn $INSTALLDIR ~/.vim
+fi
+
+if [ ! -f ~/.vimrc ]; then
+	echo "Creating ~/.vimrc"
+    ln -sfn $INSTALLDIR/vimrc ~/.vimrc
+fi
+
+if [ ! -d "$INSTALLDIR/bundle" ]; then
+    echo "Now, we will create a separate directory to store the bundles Vim will use."
+    mkdir "$INSTALLDIR/bundle"
+    mkdir -p "$INSTALLDIR/tmp/backup" "$INSTALLDIR/tmp/swap" "$INSTALLDIR/tmp/undo"
+fi
+
+if [ ! -d "$INSTALLDIR/bundle/vundle" ]; then
+    echo "Then, we install Vundle (https://github.com/VundleVim/Vundle.vim)."
+	git clone https://github.com/VundleVim/Vundle.vim.git $INSTALLDIR/bundle/Vundle.vim
+
+fi
+
+echo "Installing all plugins"
+vim +BundleInstall +qall 
+
